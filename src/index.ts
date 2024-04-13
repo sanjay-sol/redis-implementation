b1cf9ce1b336536fb5a984e821a09f4211864908
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-04-14 02:21:14 +0530

    simple setter and getter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
 import * as net from "node:net";
-
+import Parser from "redis-parser";
+const mem : any= {};
 const server: net.Server = net.createServer((connection: net.Socket) => {
     console.log("Client connected...");
     connection.on("data", (data: Buffer) => {
-        console.log(data.toString());
-        connection.write('+OK\r\n');
+        const parser = new Parser({
+            returnReply: (reply : any ) =>  {
+                console.log("reply-->", reply);
+                const type = reply[0];
+                switch (type) {
+                    case 'set': {
+                        const key = reply[1];
+                        const value = reply[2];
+                        mem[key] = value;
+                        connection.write('+OK\r\n');
+                    }
+                    break;
+                    case 'get': {
+                        const key = reply[1];
+                        const value = mem[key];
+                        console.log("value-->", value);
+                        if (!value) {
+                            connection.write('$-1\r\n');
+                        } else {
+                            connection.write(`$${value.length}\r\n${value}\r\n`);
+                        }
+                    }
+                }
+                // connection.write(reply);
+            },
+            returnError: (err: Error) => {
+                console.error("error-->",err);
+            },
+        });
+        parser.execute(data);
+        // connection.write('+OK\r\n');
     });
 });
 
